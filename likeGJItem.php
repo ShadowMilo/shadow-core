e3ceec714e9ff622667b94b9fe58775ba5977d4c
ShadowMilo <djsnyder126@gmail.com>
2026-08-07 13:51:36 -0400

    liking levels
    Implement like functionality for levels with secret check.

---
+++ likeGJItem.php
@@ -0,0 +1,15 @@
+<?php
+require "incl/connection.php";
+require "incl/config.php";
+// secret check
+if (!isset($_POST["secret"]) || $_POST["secret"] !== "Wmfd2893gb7") {
+    exit("-1, not the secret");
+}
+// get level id
+$levelID = int($_POST["levelID"]);
+$query = $db->prepare("
+// update db
+$query = $db->prepare("UPDATE levels SET likes = likes + 1 WHERE levelID = :levelID");
+$query->execute([":levelID" => $levelID]);
+echo "1";
+?>
