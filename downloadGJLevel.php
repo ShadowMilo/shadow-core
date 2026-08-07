0fb4da8ae79e10ae7b1532551f79ef97d03348f2
ShadowMilo <djsnyder126@gmail.com>
2026-08-07 13:03:50 -0400

    db consistency

--- downloadGJLevel.php
+++ downloadGJLevel.php
@@ -4,18 +4,18 @@ include "incl/gdpsLib.php";
 
 // Secret
 if (!isset($_POST["secret"]) || $_POST["secret"] != "Wmfd2893gb7") {
-    exit("-1");
+    exit("-1, not the secret");
 }
 
 // Level ID
 if (!isset($_POST["levelID"])) {
-    exit("-1");
+    exit("-1, give level id pls");
 }
 
 $levelID = $_POST["levelID"];
 
 // Xan was here.
-$query = $conn->prepare("SELECT * FROM gjLevels WHERE levelID = :levelID");
+$query = $conn->prepare("SELECT * FROM levels WHERE levelID = :levelID");
 $query->execute([":levelID" => $levelID]);
 $level = $query->fetch(PDO::FETCH_ASSOC);
 
@@ -24,11 +24,11 @@ $path = "levels/$levelID";
 if (file_exists($path)) {
     $levelstring = file_get_contents($path);
 } else {
-    exit("-1");
+    exit("-1, not a level");
 }
 
 // Level Object
-$response = "1:{$level["levelID"]}:2:{$level["levelName"]}:3:".GDPS::base64url_decode($level["description"]).":4:$levelstring:5:{$level["levelVersion"]}:6:{$level["userID"]}:10:{$level["downloads"]}:14:{$level["likes"]}:15:{$level["length"]}";
+$response = "1:{$level["levelID"]}:2:{$level["name"]}:3:".GDPS::base64url_decode($level["description"]).":4:$levelstring:5:{$level["version"]}:6:{$level["userID"]}:10:{$level["downloads"]}:14:{$level["likes"]}:15:{$level["length"]}";
 
 echo $response;
 ?>
