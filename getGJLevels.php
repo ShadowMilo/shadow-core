c2e46a73dcb3c2fa1d394fcc21b05b62d0f3b0ca
ShadowMilo <djsnyder126@gmail.com>
2026-08-09 13:57:05 -0400

    so i somehow deleted half my code?

--- getGJLevels.php
+++ getGJLevels.php
@@ -66,3 +66,88 @@ switch ($type) {
         exit("-1, thats not a search type silly");
         
 }
+// difficulty filters
+$diffSql = null;
+if ($diff != "-") {
+    $diffs = explode(",", $diff);
+
+    if (isset($where)) {
+        $diffSql = " AND (";
+    } else {
+        $diffSql = " WHERE (";
+    }
+
+    $diffSql .= "(";
+    foreach ($diffs as $difficulty) {
+        if ($difficulty != -1) {
+            $diffnum = "{$difficulty}0";
+        } else {
+            $diffnum = 0;
+        }
+        if ($difficulty == $diffs[array_key_last($diffs)]) {
+            $diffSql .= "diff = {$diffnum}";
+        } else {
+            $diffSql .= "diff = {$diffnum} OR ";
+        }
+    }
+    $diffSql .= ")";
+}
+//that stuff was taken from MirrorCore, got zero clue how that works.
+
+
+//this probably fetches the levels
+$query = $conn->prepare("SELECT * FROM levels $where $diffSql $order LIMIT 10 OFFSET {$page}0");
+$query->execute();
+$levelResult = $query->fetchAll();
+
+//are there even levels?
+if (empty($levelResult)) {
+    exit("-1, theres no levels here");
+}
+
+//how many levels?
+if ($realLevelCount) {
+    $query = $conn->prepare("SELECT COUNT(*) FROM levels $where $diffSql");
+    $query->execute();
+    $amountOfLevels = $query->fetchColumn();
+}
+
+//create the response
+//gd is stupid so this is weird
+$response = "";
+
+//level object
+//why is this in the php and not in the client
+$levelObject = null;
+$creatorObject = null;
+foreach ($levelResult as $row) {
+    // i guess we're doing fractions or something????
+    if ($row["diff"] == 0) {
+        $difficultyDenominator = 0;
+    } else {
+        $difficultyDenominator = 10;
+    }
+
+    //parameter time :)
+    $levelObject .= "1:{$row['levelID']}:2:{$row['name']}:3:{$row['description']}:5:{$row['version']}:6:{$row['userID']}:8:{$difficultyDenominator}:9:{$row['diff']}:10:{$row['downloads']}:11:0:12:{$row['song']}:13:{$row['gameVersion']}:14:{$row['likes']}:15:{$row['length']}|";
+    $creatorObject .= "{$row['userID']}:{$row['username']}|";
+}
+$levelObject = substr($levelObject, 0, -1);
+$creatorObject = substr($creatorObject, 0, -1);
+$response .
+    = "{$levelObject}#{$creatorObject}#";
+// allat for *one level*
+// i hate gd sometimes
+
+//Page Object
+//whyyyyyyyy
+if ($realLevelCount) {
+    $response .= "{$amountOfLevels}";
+} else {
+    $response .= "9999";
+}
+$response .= ":{$page}0:".count($levelResult);
+
+//and finally...
+echo $response;
+?>
