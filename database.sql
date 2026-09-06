aa1d8c2ecaf046071c7353ca48622e0119b95dde
ShadowMilo <djsnyder126@gmail.com>
2026-09-06 10:33:55 -0400

    get rid of the test data

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-shadowmilo.alwaysdata.net
--- Generation Time: Sep 06, 2026 at 04:20 PM
+-- Generation Time: Sep 06, 2026 at 04:31 PM
 -- Server version: 11.4.13-MariaDB
 -- PHP Version: 8.4.24
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `shadowmilo_gdps`
 --
-CREATE DATABASE IF NOT EXISTS `shadowmilo_gdps` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `shadowmilo_gdps`;
 
 -- --------------------------------------------------------
 
@@ -50,19 +48,6 @@ CREATE TABLE `levels` (
   `trendingScore` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `levels`
---
-
-INSERT INTO `levels` (`levelID`, `name`, `description`, `username`, `userID`, `udid`, `likes`, `downloads`, `gameVersion`, `version`, `timestamp`, `length`, `diff`, `stars`, `featureType`, `featureScore`, `song`, `trendingScore`) VALUES
-(100, 'test', '', 'ShadowMilo', 1, 'ffffff-a23-cg3-hgdmasj', 0, 3, 1, 1, 213640, 1, 10, 5, 1, 10, 0, -17),
-(101, 'shadow hello', 'hellothere', 'test', 71, 'ffffff-a23-cg2-hgddbei', 10, 1001, 1, 2, 173018, 3, 30, 3, 0, 0, 2, -19),
-(102, 'you a poopie pants', '', 'Player', 0, '', 1, 0, 1, 1, 0, 0, 20, 3, 1, 100, 0, -14),
-(103, 'triple t god', '', 'Player', 0, '', 1, 0, 1, 1, 0, 0, 40, 3, 1, 1, 0, -10),
-(104, 'p', '', 'Player', 0, '', 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, -10),
-(105, 'p', '', 'Player', 0, '', 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, -2),
-(106, 'hey poop', '', 'hi', 0, '', 1, 1, 1, 1, 0, 0, 40, 7, 0, 50, 0, 6);
-
 -- --------------------------------------------------------
 
 --
@@ -86,13 +71,6 @@ CREATE TABLE `roles` (
   `rateLevels` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`id`, `name`, `rateLevels`) VALUES
-(1, 'admin', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -104,13 +82,6 @@ CREATE TABLE `users` (
   `udid` text NOT NULL,
   `role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`username`, `udid`, `role`) VALUES
-('Player', 'ffffffff-f086-89f7-0033-c5870033c587', 0);
 
 --
 -- Indexes for dumped tables
@@ -136,13 +107,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `levels`
 --
 ALTER TABLE `levels`
-  MODIFY `levelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `levelID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
